13ce5732c8ac7b94323369538287ccce190e6d0d
mike <mikesapienzaaudio@gmail.com>
2021-02-04 18:29:46 -0600

    added navigation on demo app

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import ComponentsScreen from "./ComponentsScreen";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Testing..check 2</Text>;
+const HomeScreen = (props) => {
+  return (
+    <View>
+      <Text style={styles.text}>Welcome to the demo app</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => props.navigation.navigate("Components")}
+      />
+      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
+        <Text>Go to list demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
